a59edbb30524cc0aad8561f44c230fe09bec3fc0
Lara <laraellenh@gmail.com>
2022-01-19 10:34:58 -0500

    rendered images from get request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+getData()
+function getData() {
+    // fetch
+    fetch(imgUrl)
+    // /parse
+        .then(r => r.json())
+        // make an object, 
+        .then(dataObject => {
+            // object.message message is the key (the dog url is the value)
+            dataObject.message.forEach(dogURL => {
+                const dogCon = document.getElementById("dogImageContainer")
+                const img = document.createElement("img")
+
+                img.src = dogURL
+                dogCon.append(img)
+            })
+        })
+}
